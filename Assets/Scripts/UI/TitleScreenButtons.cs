9c6b6d809befbf9555783bdfef1241254b7c5264
HectorP55 <hectorp55@tamu.edu>
2020-05-12 14:57:20 -0500

    Adding title screen with start button

---
+++ Assets/Scripts/UI/TitleScreenButtons.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenButtons : MonoBehaviour
+{
+    public void StartGame() {
+        LoadScene("Main");
+    }
+
+    private void LoadScene(string sceneName) {
+        SceneManager.LoadScene("Main");
+    }
+}

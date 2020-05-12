20bd04913674823d7c0c34962fce1b15914840a5
HectorP55 <hectorp55@tamu.edu>
2020-05-12 16:09:02 -0500

    Added retry screen

---
+++ Assets/Scripts/UI/RetryScreenButtons.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RetryScreenButtons : MonoBehaviour
+{
+    public void RetryGame() {
+        LoadScene("MainGame");
+    }
+
+    private void LoadScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+}

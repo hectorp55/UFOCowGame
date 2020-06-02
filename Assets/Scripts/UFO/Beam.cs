afae24f5c75d70d503105ee5c92a57f360c2bcb8
HectorP55 <hectorp55@tamu.edu>
2020-06-01 21:22:10 -0500

    Adding BG into main game and mission screen

--- Assets/Scripts/UFO/Beam.cs
+++ Assets/Scripts/UFO/Beam.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Beam : MonoBehaviour
 {
-    public const float liftSpeed = 6f;
+    public const float liftSpeed = 5f;
 
     public bool isCowInRange;
     public Rigidbody2D cowToLift;
